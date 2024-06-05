73943cca092c30811109a1c3f6559c6360a08368
Tristan Nobre <nobretristan@gmail.com>
2024-06-05 09:30:21 +0200

    working

--- quiz-api/routes/quiz_routes.py
+++ quiz-api/routes/quiz_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Response
 from flask_cors import cross_origin
 import database.quiz_db as quiz_db
 from utils.converters import questionToJSON, JSONToQuestion
@@ -6,33 +6,43 @@ from utils.converters import questionToJSON, JSONToQuestion
 quiz_bp = Blueprint('quiz', __name__)
 
 @quiz_bp.route('/quiz-info', methods=['GET', 'OPTIONS'])
-@cross_origin()
 def GetQuizInfo():
-    return quiz_db.get_quiz_info(), 200
+	return quiz_db.get_quiz_info(), 200
 
 @quiz_bp.route('/questions/<int:question_id>', methods=['GET'])
 def GetQuestion(question_id):
-    question = quiz_db.get_question(question_id)
-    if question is None:
-        return {"message": "Question not found"}, 404
-    
-    return questionToJSON(question), 200
+	question = quiz_db.get_question(question_id)
+	if question is None:
+		return {"message": "Question not found"}, 404
+		
+	return questionToJSON(question), 200
 
 @quiz_bp.route('/questions', methods=['GET'])
 def GetQuestionByPosition():
-    position = request.args.get('position')
-    if position is None:
-        return {"message": "Position not provided"}, 400
-    question = quiz_db.get_question_by_position(position)
-    if question is None:
-        return {"message": "Question not found"}, 404
-    
-    return questionToJSON(question), 200
+	position = request.args.get('position')
+	if position is None:
+		return {"message": "Position not provided"}, 400
+	question = quiz_db.get_question_by_position(position)
+	if question is None:
+		return {"message": "Question not found"}, 404
+		
+	return questionToJSON(question), 200
 
-@quiz_bp.route('/participations', methods=['POST'])
+@quiz_bp.route('/participations', methods=['POST', 'OPTIONS'])
+@cross_origin()
 def PostParticipation():
-    payload = request.get_json()
-    playerName = payload['playerName']
-    answers = payload['answers']
-    
-    return quiz_db.insert_participation(playerName, answers) 
+	if request.method == 'OPTIONS':
+		return _build_cors_preflight_response()
+		
+	payload = request.get_json()
+	playerName = payload['playerName']
+	answers = payload['answers']
+		
+	return quiz_db.insert_participation(playerName, answers), 200
+
+def _build_cors_preflight_response():
+    response = Response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    return response
