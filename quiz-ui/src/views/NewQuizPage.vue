70e4b9269061b46b05134b059415127ff658783a
MathisPipart <117630102+MathisPipart@users.noreply.github.com>
2024-06-05 10:51:16 +0200

    Design New Quiz OK

--- quiz-ui/src/views/NewQuizPage.vue
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -25,11 +25,13 @@ export default {
 
 <template>
   <div class="new-quiz">
-    <h1>Nouvelle page de quiz</h1>
+    <h1>Lancement du Quiz</h1>
     <br>
-    <p>Saisissez votre nom :</p>
-    <input type="text" v-model="username" placeholder="Username" class="form-control" />
-    <button type="submit" class="btn btn-success mt-2" @click="launchNewQuiz">GO!</button>
+    <div class="BoiteName">
+      <p>Saisissez votre nom :</p>
+      <input type="text" v-model="username" placeholder="Username" class="form-control" />
+      <button type="submit" class="btn btn-success mt-2" @click="launchNewQuiz">GO!</button>
+    </div>
   </div>
 </template>
 
@@ -38,5 +40,31 @@ export default {
   width: 20%;
   padding: 5px;
   margin-right: 10px;
+}
+
+.BoiteName {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 800px;
+  max-height: 250px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  border-radius: 15px;
+  padding: 20px;
+  margin: 0 auto;
+  margin-top: 5%;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.BoiteName .form-control {
+  text-align: center;
+  width: 50%;
+}
+.BoiteName .form-control:focus {
+  border-color: #a5ada1;
+  box-shadow: 0 0 5px rgba(144, 164, 143, 0.8);
 }
 </style>
